523aed79909494b7557a697bcb453f6969210ecf
marcelo.silva <marcelo.silva@neomind.com.br>
2022-11-12 20:30:44 -0300

    Criado configurações do projeto criado classes de configuração do kafka criado service e controller do projeto

---
+++ str-producer/src/main/java/com/marcelo/strpeoducer/services/StringProducerService.java
@@ -0,0 +1,28 @@
+package com.marcelo.strpeoducer.services;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@RequiredArgsConstructor
+@Service
+public class StringProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message){
+        kafkaTemplate.send("str-topic", message).addCallback(
+                success-> {
+                    if(success != null){
+                        log.info("Message Sended With Succes {}", message);
+                        log.info("Partition {}", success.getRecordMetadata().partition());
+                        log.info("Offset {}", success.getRecordMetadata().offset());
+                    }
+                },
+                error -> log.error("Error to send message")
+        );
+    }
+}
