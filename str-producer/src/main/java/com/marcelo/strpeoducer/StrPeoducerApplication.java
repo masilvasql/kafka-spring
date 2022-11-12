1ee7cd810cc692b3d6787a98eed1cb23387dd13d
marcelo.silva <marcelo.silva@neomind.com.br>
2022-11-12 19:07:40 -0300

    initial commit

---
+++ str-producer/src/main/java/com/marcelo/strpeoducer/StrPeoducerApplication.java
@@ -0,0 +1,13 @@
+package com.marcelo.strpeoducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrPeoducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrPeoducerApplication.class, args);
+    }
+
+}
---
+++ str-producer/src/test/java/com/marcelo/strpeoducer/StrPeoducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelo.strpeoducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrPeoducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
