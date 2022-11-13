eb723b736b60ab09c93f5f9cdd20c807ad610ac7
marcelo.silva <marcelo.silva@neomind.com.br>
2022-11-13 12:58:36 -0300

    Criando interface e listener customizado

--- str-consumer/src/main/java/com/marcelo/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/marcelo/strconsumer/listeners/StrConsumerListener.java
@@ -1,5 +1,6 @@
 package com.marcelo.strconsumer.listeners;
 
+import com.marcelo.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
@@ -9,25 +10,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1",
-            topicPartitions = {
-                @TopicPartition(topic = "str-topic", partitions = {"0"})
-            }
-            , containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void create(String message){
        log.info("CREATE ::: Receive Message {}", message);
     }
 
-    @KafkaListener(groupId = "group-1",
-            topicPartitions = {
-                    @TopicPartition(topic = "str-topic", partitions = {"1"})
-            }
-            , containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void log(String message){
         log.info("LOG ::: Receive Message {}", message);
     }
 
-    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-2")
     public void history(String message){
         log.info("HISTORY :::Receive Message {}", message);
     }
